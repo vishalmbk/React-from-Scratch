311644557c4f3f42c49d6a087361ae126090264b
vishalMBK <vishalbkasbe@gmail.com>
2024-03-06 21:03:08 +0530

    create Elemnts

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
-const heading = React.createElement("h1", {}, "Hello React!");
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>H1 Tag</h1>
+  </div>
+</div>; how to create this react given below */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "this is h1 tag"),
+    React.createElement("h2", {}, "this is h2 tag"),
+  ])
+);
+console.log(parent);
+const heading = React.createElement("h1", { id: "heading" }, "Hello React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
