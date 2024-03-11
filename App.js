d8ddee476da2f33ed846af3f90c54fe160ccdbb2
vishalMBK <vishalbkasbe@gmail.com>
2024-03-11 19:18:44 +0530

    npm i react

--- App.js
+++ App.js
@@ -1,32 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { ReactDOM } from "react";
 
-{
-  /* <div id="parent">
-    <div id="child">
-      <h1>H1 Tag</h1>
-      <h1>H1 Tag</h1>
-    </div>
-    </div>; 
-    <div id="parent2">
-    <div id="child2">
-      <h1>H1 Tag</h1>
-      <h1>H1 Tag</h1>
-    </div>
-    </div>; how to create this react given below */
-}
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-]);
-console.log(parent);
-const heading = React.createElement("h1", { id: "heading" }, "Hello React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const heading = React.createElement("h1", { id: "heading" }, "this is heading");
+const root = React.createRoot(document.getElementById("root"));
+root.render(heading);
