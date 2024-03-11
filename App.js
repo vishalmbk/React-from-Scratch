377abb481a90fe026a2653e5f5a8e6fbadf10cee
vishalMBK <vishalbkasbe@gmail.com>
2024-03-12 00:57:00 +0530

    comp composition

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", { id: "heading" }, "this is heading");
-// React.createelement (obj) =>reactdom createroot (obj) => (html)
-console.log(heading, "this is reactObject");
-const jsxHeading = <h1>this jsx heading</h1>;
-console.log(jsxHeading, "this is react object");
-// JSX=> babel => React.createelement (obj) =>reactdom createroot (obj) => (html)
+//Component Copmosition=>component inside copmonent
+const Title = () => {
+  return <>this title component</>;
+};
+const HeadingComp = () => {
+  return (
+    <>
+      this is heading component
+      <Title />
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComp />);
 //render method convert react obj in to html
