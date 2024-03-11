42c3396923097950597ad304adc8d3fa144d4e6d
vishalMBK <vishalbkasbe@gmail.com>
2024-03-11 19:51:36 +0530

    jsx

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 
 const heading = React.createElement("h1", { id: "heading" }, "this is heading");
-const root = React.createRoot(document.getElementById("root"));
-root.render(heading);
+// React.createelement (obj) =>reactdom createroot (obj) => (html)
+console.log(heading, "this is reactObject");
+const jsxHeading = <h1>this jsx heading</h1>;
+console.log(jsxHeading, "this is react object");
+// JSX=> babel => React.createelement (obj) =>reactdom createroot (obj) => (html)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+//render method convert react obj in to html
