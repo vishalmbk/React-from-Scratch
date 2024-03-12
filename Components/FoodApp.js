6ccd008b4ac303a8c41962c71cbd8e49ae730d94
vishalMBK <vishalbkasbe@gmail.com>
2024-03-13 05:03:55 +0530

    res-card-static

--- Components/FoodApp.js
+++ Components/FoodApp.js
@@ -1,11 +1,19 @@
 //- food app -
-//- Navbar -
-//- nav logo, nav menu
-//-home,about,contact,cart
-//- body -
-//- restraurents, rescard ,resmenu
-//- Footer -
-//- copyrights, Links, Address,Contact
+//- *Header* -
+//  - nav logo
+//  - nav items
+//     -home,about,contact,cart
+//- *body* -
+//  -Search
+//  -RestraurentContainer
+//     -resCard
+//       -img
+//       -Name of res, star ratings, cuisine ,etc
+//- *Footer* -
+//  - copyrights
+//  -Links
+//  -Address
+//  -Contact
 
 import React from "react";
 
@@ -15,7 +23,8 @@ const Header = () => {
       <div className="logo-container">
         <img
           className="logo"
-          src="https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png"
+          src="https://cdn.vectorstock.com/i/preview-1x/73/82/food-logo-vector-38377382.jpg"
+          alt="Logo"
         />
       </div>
       <div className="nav-items">
@@ -29,11 +38,60 @@ const Header = () => {
     </div>
   );
 };
+const RestaurentCard = () => {
+  return (
+    <div className="res-container">
+      <div className="res-card">
+        <h2>Priti Food</h2>
+        <img
+          className="res-logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDPGr_sA0u2WwHd_xKm5bxDYDvso5M0f9Wyg&usqp=CAU"
+        />
+        <h3>Shahi Paneer, Biryani, South Indian, North Indian</h3>
+        <h4>4.5 stars...</h4>
+        <h4>15 minutes</h4>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+        <RestaurentCard />
+      </div>
+    </div>
+  );
+};
+
 const FoodApp = () => {
   return (
     <>
-      this is food app
       <Header />
+      <Body />
     </>
   );
 };
