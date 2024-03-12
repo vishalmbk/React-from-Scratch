ec1528abdbcff0673fc729c209cc56adca54bffc
vishalMBK <vishalbkasbe@gmail.com>
2024-03-12 10:11:45 +0530

    Header

--- Components/FoodApp.js
+++ Components/FoodApp.js
@@ -1,6 +1,40 @@
+//- food app -
+//- Navbar -
+//- nav logo, nav menu
+//-home,about,contact,cart
+//- body -
+//- restraurents, rescard ,resmenu
+//- Footer -
+//- copyrights, Links, Address,Contact
+
 import React from "react";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 const FoodApp = () => {
-  return <>this is food app</>;
+  return (
+    <>
+      this is food app
+      <Header />
+    </>
+  );
 };
 export default FoodApp;
